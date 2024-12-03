d80f9119120525a9ab1e1586c13233c2fcd9ce63
DovKat <dovkat1@ktu.lt>
2024-12-03 18:22:55 +0200

    once again

--- STTP_projektas/Auth/AuthEndpoints.cs
+++ STTP_projektas/Auth/AuthEndpoints.cs
@@ -55,10 +55,10 @@ public static class AuthEndpoints
             
             var cookieOptions = new CookieOptions
             {
-                HttpOnly = true,
+                HttpOnly = false,
                 SameSite = SameSiteMode.None, 
                 Expires = expiresAt,
-                Secure = false
+                Secure = true
             };
             
             httpContext.Response.Cookies.Append("RefreshToken", refreshToken, cookieOptions);
@@ -106,10 +106,10 @@ public static class AuthEndpoints
 
             var cookieOptions = new CookieOptions
             {
-                HttpOnly = true,
+                HttpOnly = false,
                 SameSite = SameSiteMode.None, 
                 Expires = expiresAt,
-                Secure = false
+                Secure = true
             };
             
             httpContext.Response.Cookies.Append("RefreshToken", newRefreshToken, cookieOptions);
