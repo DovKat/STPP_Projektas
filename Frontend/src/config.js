c520dc69f4b039b56ca35632b7b98b9f38152088
DovKat <dovkat1@ktu.lt>
2024-12-04 15:32:10 +0200

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // API Base URL for your backend
-    apiBaseUrl: 'sttp-api-htg5d3hycffqbwcv.westeurope-01.azurewebsites.net', // Change this to your actual backend URL
+    apiBaseUrl: 'https://www.sttp-api-htg5d3hycffqbwcv.westeurope-01.azurewebsites.net', // Change this to your actual backend URL
   };
   
   export default config;
